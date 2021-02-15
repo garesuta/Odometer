6a515f3b70285fb34de99fdc1c1105681629632f
garesuta <garesuta@gmail.com>
2021-02-15 22:02:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/odometer/OdometerService.java
@@ -0,0 +1,27 @@
+package com.example.odometer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.Binder;
+import java.util.Random;
+
+public class OdometerService extends Service {
+    private final IBinder binder = new OdometerBinder();
+    private final Random random = new Random();
+
+    public class OdometerBinder extends Binder{
+        OdometerService getOdometer(){
+            return OdometerService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    public double getDistance(){
+        return random.nextDouble();
+    }
+}
