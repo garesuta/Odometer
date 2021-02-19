a3bcf82660d52ef63c9b294801f545eb8437920a
garesuta <garesuta@gmail.com>
2021-02-19 23:26:21 +0700

    made user respond request permision

--- app/src/main/java/com/example/odometer/MainActivity.java
+++ app/src/main/java/com/example/odometer/MainActivity.java
@@ -12,14 +12,19 @@ import android.os.Handler;
 import android.widget.TextView;
 import java.util.Locale;
 import android.content.pm.PackageManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 
+import androidx.core.app.NotificationCompat;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
     private OdometerService odometer;
     private boolean bound = false;
     private final int PERMISSION_REQUEST_CODE = 698;
+    private final int NOTIFICATION_ID = 423;
 
     private ServiceConnection connection = new ServiceConnection() {
         @Override
@@ -39,6 +44,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         displayDistance();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[],int[]grantResults){
+
+        switch (requestCode){
+            case PERMISSION_REQUEST_CODE:{
+                if (grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    Intent intent = new Intent(this, OdometerService.class);
+                    bindService(intent,connection,Context.BIND_AUTO_CREATE);
+                }else {
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                            .setSmallIcon(android.R.drawable.ic_menu_compass)
+                            .setContentTitle(getResources().getString(R.string.app_name))
+                            .setContentText(getResources().getString(R.string.permission_denied))
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .setVibrate(new long[]{1000,1000})
+                            .setAutoCancel(true);
+                    Intent actionIntent = new Intent(this, MainActivity.class);
+                    PendingIntent actionPendingIntent = PendingIntent.getActivity(this,0,actionIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+                    builder.setContentIntent(actionPendingIntent);
+                    NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+                    notificationManager.notify(NOTIFICATION_ID,builder.build());
+                }
+            }
+        }
     }
     @Override
     protected void onStart(){
